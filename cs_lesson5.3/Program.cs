ce3132e84441cd64ac7a74b93a7e6d00ca1ad23d
david <2657070472@qq.com>
2018-11-06 14:29:17 +0800

    数组

--- cs_lesson5.3/Program.cs
+++ cs_lesson5.3/Program.cs
@@ -10,6 +10,16 @@ namespace cs_lesson5._3
     {
         static void Main(string[] args)
         {
+            string[] friendNames = new string[3] { "Robert Barwell", "Mike Parry", "Jeremy Beacock"};
+            // string[] friendNames = { "Robert Barwell", "Mike Parry", "Jeremy Beacock"}; array definition
+            int i;
+            Console.WriteLine("Here are {0} of my friends.", friendNames.Length);
+            for (i = 0; i < friendNames.Length; i++)
+            {
+                Console.WriteLine(  friendNames[i]);
+            }
+            Console.ReadKey();
+
         }
     }
 }
